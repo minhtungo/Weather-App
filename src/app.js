e004dc090e767409071ffe09697f594379d6bfd6
Minh Tu Ngo <loducfc@gmail.com>
2021-09-06 22:17:21 -0400

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast');
 
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 //Define paths for Express config
 const viewsPath = path.join(__dirname, '../templates/views');
@@ -86,6 +87,6 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log("listening on port 3000");
+app.listen(port, () => {
+    console.log("listening on port " + port");
 });
